066aa2343def7520b037cd7623ac3eae11e927e9
Surya N <125745636+itzsurya@users.noreply.github.com>
2024-01-11 21:10:05 +0530

    Add files via upload

---
+++ Surya-Portfolio/main.js
@@ -0,0 +1,36 @@
+var typed=new Typed(".text",{
+    strings:["Techie","Mobile Application Developer", "learner", "Gamer"],
+    typeSpeed:100,
+    backspeed:100,
+    backDelay:1000,
+    loop:true
+});
+
+
+
+
+
+
+
+
+/*const btn1 = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn1.value = 'Sending...';
+
+   const serviceID = 'service_4r0mdxd';
+   const templateID = 'template_tmj5she';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn1.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn1.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
+*/
